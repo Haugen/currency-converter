5138e0384521dc33dd77730f35d0e0f384b06217
Tobias Haugen <hej@tobiashaugen.se>
2018-11-12 17:15:01 -0300

    Added a currency map for later usage.

---
+++ src/utility/currencyMap.js
@@ -0,0 +1,9 @@
+export const currencyMap = {
+  ARS: 'Argentine peso',
+  COP: 'Colombian peso',
+  EUR: 'Euro',
+  HKD: 'Hong Kong dollar',
+  NOK: 'Norwegian krone',
+  SEK: 'Swedish krona',
+  USD: 'United States Dollar'
+};
